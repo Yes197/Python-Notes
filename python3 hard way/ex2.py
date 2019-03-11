1511dfe4a45f867f22e4bff4bb26daac735edc25
Yes197 <yesicaaws@gmail.com>
2019-03-11 11:54:25 -0500

    commit python hardway exercises

---
+++ python3 hard way/ex2.py
@@ -0,0 +1,9 @@
+#A comment, this is so you can read your program later.
+#A #Anything after the # is ignores by python.
+
+print("I coyld have code like this.") #and the comment after
+
+# You can also use a comment to "disable or comment out the code
+# print("This won't run.")
+
+print("This will run.")
