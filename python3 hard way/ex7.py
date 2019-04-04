99d2e77f65aafdd6fc71f25bbb4caaa8734115d8
Yes197 <yesicaaws@gmail.com>
2019-04-04 17:05:49 -0500

    Adding more excercises commit

--- python3 hard way/ex7.py
+++ python3 hard way/ex7.py
@@ -1,5 +1,5 @@
 print("Mary has a little lamb.")
-print("Its fleece was white as {}.".format('snow')) #snow no es una variable sino un string con la palabra snow dentro una variable no tiene '' alrededor
+print("Its fleece was white as {}.".format('snow')) #snow no es una variable sino un string con la palabra snow dentro, una variable no tiene '' alrededor
 print("And everywhere that Mary went.") #Tipicamente usas '' para cualquier string corto como 'snow'
 print("." * 10) #Repite 10 veces el string "."
 
@@ -17,5 +17,5 @@ end11 = "e"
 end12 = "r"
 
 #Mira esa coma al final. Trata de removerlo para ver que pasa
-print(end1 + end2 + end3 + end4 + end5 + end6, end=' ') #Para que continue en la siguiente línea
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ') #Para que continue en la siguiente línea sin salto de línea
 print(end7 + end8 + end9 + end10 + end11 + end12)
--- python3 hard way/ex8.py
+++ python3 hard way/ex8.py
@@ -1,12 +1,13 @@
-formatter = "{} {} {} {}"
+formatter = "{} {} {} {}" #Esta línea es una variable que contiene llaves que se usarán mas adelante para colocar elementos dentro, ya sean strings, integers. Hay un elemento por cada llave.
 
-print(formatter.format(1, 2, 3, 4))
-print(formatter.format("one", "two", "three", "four"))
-print(formatter.format(True, False, False, True))
-print(formatter.format(formatter, formatter, formatter, formatter))
+#A continuación usaremos una "función" para convertir la variable "formatter" en otros elementos. El formatter.format() toma el string formatter de la primera linea y luego llama a la función format y pasa al formato 4 argumentos, por las 4 {} de la variable formatter. El resultado de llamar la función format en formatter es un nuevo string que tiene las {} reemplazadas con 4 variables. Esto es lo que print imprime al final.
+print(formatter.format(1, 2, 3, 4)) #estos son los elementos en este caso, integers
+print(formatter.format("one", "two", "three", "four")) #En este caso un string, cada string abarca todo lo que esté dentro de "" ya sea 1 o más palabras.
+print(formatter.format(True, False, False, True)) #Los elementos también pueden ser booleans.
+print(formatter.format(formatter, formatter, formatter, formatter)) #Estos elementos inclusive pueden ser variables, en este caso usamos la misma variable "formatter".
 print(formatter.format(
     "Try your",
     "Own text here",
     "Maybe a poem",
     "Or a song about fear"
-))
+)) #Este es un ejemplo de que en cada espacio pueden ir variables palabras.
